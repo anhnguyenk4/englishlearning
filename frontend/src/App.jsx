ddfc34f45f4aa0b6b12e1ce751bcec4f3877744e
anhnguyenk4 <kayn28120411@gmail.com>
2026-03-30 13:35:04 +0700

    update code 3

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to English Learning App</h1>
+      <p>This is a basic React app with Vite.</p>
+    </div>
+  )
+}
+
+export default App
